69872aa55b0457b7ea33da3841b5386250443f0e
cpreinholtz <cpr978@gmail.com>
2019-12-07 17:58:13 -0800

    Add untested scene generation function

---
+++ scene_generation.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+
+
+def generateDummyScene(numRows = 5, numCols = 5):
+  dummyScene = np.zeros((5,5), dtype = 'int16')
+  
+  #row , col
+  dummyScene[2,2]   = 4
+  dummyScene[1,1:3] = 2
+  dummyScene[3,2] = 2
+  return dummyScene
+
+  #  01234
+  #  _____
+  #0|
+  #1| 222
+  #2|  4
+  #3|  2
